8d9ec10c9ca5764289f0bd3d6ef38a49a3d17c03
jwald1 <email@example.com>
2017-05-02 17:43:49 -0400

    First pending acceptance spec

---
+++ app/api.rb
@@ -0,0 +1,11 @@
+module ExpenseTracker
+  class API < Sinatra::Base
+    post '/expenses' do
+      JSON.generate('expense_id' => 42)
+    end
+
+    get '/expenses/:date' do
+      JSON.generate([])
+    end
+  end
+end
---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+require 'bundler'
+Bundler.require
+
+require 'json'
+require_all 'app'
